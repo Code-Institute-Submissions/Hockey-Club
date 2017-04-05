364bdf584e6366ad09ecbe2f7c0ec76d6bb14014
James Horan <horan5034@gmail.com>
2017-04-05 12:45:17 +0100

    update carousel directive

--- js/directive/carousel.js
+++ js/directive/carousel.js
@@ -3,7 +3,7 @@ var app = angular.module("CarouselDir", []);
 app.directive("carousel", function(){
 	return{
 		restrict	: "A", 
-		templateUrl : "html/directive/carousel.html",
+		templateUrl : "/Hockey-Club/html/directive/carousel.html",
 		controller  : "MediaController"
 	};
 });
