5303fc4a1dd736ea6d661ef6a75fbb369b2edac3
Horan5034 <horan5034@gmail.com>
2017-04-05 19:33:25 +0100

    Nav routing

--- js/app.js
+++ js/app.js
@@ -7,35 +7,35 @@ angular.module("hockey-club").config( function($locationProvider, $routeProvider
 	$locationProvider.html5Mode(true);
 
 	$routeProvider.when('/', {
-		templateUrl: 'html/home.html',
+		templateUrl: '/Hockey-Club/html/home.html',
 		controller : 'HomeController'
 	})
 	.when('/teams/mens/first', {
-		templateUrl: 'html/teams/mens-first.html',
+		templateUrl: '/Hockey-Club/html/teams/mens-first.html',
 		controller : 'TeamController'
 	})
 	.when('/teams/mens/second', {
-		templateUrl: 'html/teams/mens-second.html',
+		templateUrl: '/Hockey-Club/html/teams/mens-second.html',
 		controller : 'TeamController'
 	})
 	.when('/teams/ladies/first', {
-		templateUrl: 'html/teams/ladies-first.html',
+		templateUrl: '/Hockey-Club/html/teams/ladies-first.html',
 		controller : 'TeamController'
 	})
 	.when('/teams/ladies/second', {
-		templateUrl: 'html/teams/ladies-second.html',
+		templateUrl: '/Hockey-Club/html/teams/ladies-second.html',
 		controller : 'TeamController'
 	})
 	.when('/teams/juniors', {
-		templateUrl: 'html/teams/juniors.html',
+		templateUrl: '/Hockey-Club/html/teams/juniors.html',
 		controller : 'TeamController'
 	})
 	.when('/enquiry', {
-		templateUrl: 'html/enquiry.html',
+		templateUrl: '/Hockey-Club/html/enquiry.html',
 		controller : 'EnquiryController'
 	})
 	.when('/media/images', {
-		templateUrl: 'html/media/image-carousel.html',
+		templateUrl: '/Hockey-Club/html/media/image-carousel.html',
 		controller : 'MediaController'
 	});
 });
