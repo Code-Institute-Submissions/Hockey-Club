7c137aa53ce49d08a223c1fd16e34b765bbff755
Horan5034 <horan5034@gmail.com>
2017-04-05 19:31:18 +0100

    Image source refs

--- js/directive/navbar.js
+++ js/directive/navbar.js
@@ -3,7 +3,7 @@ var app = angular.module("NavDir", []);
 app.directive("navbar", function(){
 	return{
 		restrict	: "A", 
-		templateUrl : "html/directive/navbar.html",
+		templateUrl : "/Hockey-Club/html/directive/navbar.html",
 		controller  : "HomeController"
 	};
 });
