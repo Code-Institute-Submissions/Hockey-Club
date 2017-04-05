4fc9d39d9b665ee008175c0f9e8dc3101adc52b5
Horan5034 <horan5034@gmail.com>
2017-04-05 19:14:33 +0100

    Pushed to gh-pages

--- js/directive/navbar.js
+++ js/directive/navbar.js
@@ -3,7 +3,7 @@ var app = angular.module("NavDir", []);
 app.directive("navbar", function(){
 	return{
 		restrict	: "A", 
-		templateUrl : "/Hockey-Club/html/directive/navbar.html",
+		templateUrl : "html/directive/navbar.html",
 		controller  : "HomeController"
 	};
 });
